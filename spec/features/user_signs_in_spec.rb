c925c8d3fcba9685e139e8a2a8f5f67d42daeee5
Lawrence Chang <lc.uclakers@gmail.com>
2014-07-05 11:45:20 -0400

    adjusted left of launcher info div, began rspec for user login

---
+++ spec/features/user_signs_in_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+feature 'User signs in', %Q{
+  As a authenticated user
+  I want to sign in
+  So that I can continue to my profile page
+} do
+
+  scenario 'user signs in with facebook' do
+  end
+end
