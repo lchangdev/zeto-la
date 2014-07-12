871581c18a05bd570f16a3032ddd13191ef10929
Lawrence Chang <lc.uclakers@gmail.com>
2014-07-12 18:28:46 -0400

    launch-info div now able to toggle between events and users

--- app/assets/javascripts/fade.js
+++ app/assets/javascripts/fade.js
@@ -1,4 +1,18 @@
 $(document).ready(function(){
+  $('#events-container').hide();
+
+  $('#events-btn').on('click', function(e){
+    $('#events-container').show();
+    $('.launch-info-users').hide();
+    e.preventDefault();
+  })
+
+  $('#users-btn').on('click', function(e){
+    $('.launch-info-users').show();
+    $('#events-container').hide();
+    e.preventDefault();
+  })
+
   $('.enter-btn, .initial-enter-btn').mouseenter(function(){
     $(this).fadeTo('medium', 0.6);
   });
